60cbab5cb11a1671d01a61f98ce74020316b5796
Erik Ylipää <erik.ylipaa@gmail.com>
2011-04-05 13:32:30 +0200

    la till board id till field och började ändra i board/show

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110405104231) do
+ActiveRecord::Schema.define(:version => 20110405112127) do
 
   create_table "boards", :force => true do |t|
     t.string   "name"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(:version => 20110405104231) do
     t.integer  "height"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "board_id"
   end
 
   create_table "notes", :force => true do |t|
