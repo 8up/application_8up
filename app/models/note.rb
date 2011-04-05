4547594d07dfae6145a8e1ae1843e7b8520d364c
Erik Ylipää <erik.ylipaa@gmail.com>
2011-04-05 13:02:35 +0200

    Lade till relationer mellan board och field, samt field och notes

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,2 +1,3 @@
 class Note < ActiveRecord::Base
+  belongs_to :field
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110405094014) do
+ActiveRecord::Schema.define(:version => 20110405104231) do
 
   create_table "boards", :force => true do |t|
     t.string   "name"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(:version => 20110405094014) do
     t.integer  "owner_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "field_id"
   end
 
   create_table "users", :force => true do |t|
