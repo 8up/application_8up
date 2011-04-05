512621964d0cc00ff0e46120831f99bc108093d2
jtn <tjelle@gmail.com>
2011-04-05 08:41:23 +0200

    Created rails application.

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Application8up::Application.config.session_store :cookie_store, :key => '_application_8up_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Application8up::Application.config.session_store :active_record_store
