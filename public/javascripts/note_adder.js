a8191f7ea522d1134f022aaedb42f4d36182e050
Erik Ylipää <erik.ylipaa@gmail.com>
2011-04-06 12:58:38 +0200

    fixade så att note_adder bara gör sin grej när man dubbel-klickar på field:et

--- public/javascripts/note_adder.js
+++ public/javascripts/note_adder.js
@@ -1,9 +1,13 @@
 $(document).ready(function(){
-	$(".note").dblclick(function() {});
+	$("div.note").dblclick(function(e) {e.stopPropagation()});
 	$("div.field").dblclick(function(e){
+		if (e.target != this) {
+		    return true;
+		}
 		create_note(e);
 	    });
 	function create_note(e) {
+	    
 	    $(".field").append(
 			       "<div class='note' style='position:absolute; top:" + e.pageY +";left:" + e.pageX +  ";'>Foo</div>"
 			       )
