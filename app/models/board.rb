ee13be6db7d98d6f5637f28784ad16db0684882f
Alfred Andersson <alfreda@kth.se>
2011-04-07 08:18:11 +0200

    JQuery och stylesheet ändringar.

--- app/models/board.rb
+++ app/models/board.rb
@@ -2,8 +2,7 @@ class Board < ActiveRecord::Base
   has_many :fields
   def hotness
     rand(4)
-  end
-  
+  end    
   
   def trashcan
     Note.find(:all, :conditions => ["field_id = ? and trashcan = ?", self.fields, true])
