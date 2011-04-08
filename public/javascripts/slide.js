68255c1e74806a4a223680fd0cf8983664249c36
teddycow <fluffgohei@hotmail.com>
2011-04-08 09:18:05 +0200

    slide update fix 1.6

--- public/javascripts/slide.js
+++ public/javascripts/slide.js
@@ -2,5 +2,6 @@ $(document).ready(function(){
   $("#panel_arrow").click(function(){
       $("div#toolbox_area_1").toggle();
       $('#content').css('margin-left', $('#toolbox_container').width() + 'px');
+
 });
 });
