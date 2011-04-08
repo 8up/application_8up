d882b208095f03a42eb52743c47bb7a5be70e167
Erik Ylipää <erik.ylipaa@gmail.com>
2011-04-08 09:55:58 +0200

    Lade till i select.boards.js så att vi kan ta bort notes från boards

--- public/javascripts/select.board.js
+++ public/javascripts/select.board.js
@@ -3,6 +3,17 @@ $(document).ready(function(){
       $(e.target).toggleClass("selected");
   });
   $(".toolbox_button_delete").click(function(){
-    $(".selected").remove()
+	  $(".selected.note").map(function(index, domElement) 
+			     { 
+				 var note_id = $(domElement).attr('id').split('_').pop();
+				 var board_id = $(domElement).data('board_id').split('_').pop();
+				 var url_path =  "/boards/" + board_id + "/notes/" + note_id;
+						 
+				     
+				     $.ajax({url : url_path, type: 'DELETE', method: 'POST'});
+			     } );
+	  $(".selected").remove();
+	  
+	
   });
 });
