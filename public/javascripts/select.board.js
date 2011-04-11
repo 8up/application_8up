7eee1ad85fe61f000126951c1a9518c1882d3b40
Alfred Andersson <alfreda@kth.se>
2011-04-11 14:56:54 +0200

    Toolbar fix 0.2

--- public/javascripts/select.board.js
+++ public/javascripts/select.board.js
@@ -1,41 +1,54 @@
 $(document).ready(function(){
-	$("#boards_container li").click(function(e){
-
-		getData = function(id){
-			$.ajax({ 
-				url:'/boards/' + id + '.json', 
-				type: 'GET',
-				success: function(data, textStatus, jqXHR){
-
-					toolbox_board_name.text(data.board.name);
-
-				}		
-			})
-
-		};
-
-
-		$(e.target).toggleClass("selected");
-		var id = $(e.target).attr('id').split('_').pop();
-		var toolbox_board_name = $('#toolbox_board_name');
-		var toolbox_board_info = $('#toolbox_info');
-
-		if($(".selected").length==1){
-
-			getData($(".selected")[0].id.split('_')[1]);
-		}else if($(".selected").length==0){
-			toolbox_board_name.text("Name");
-		}else{
-			toolbox_board_name.text($(".selected").length + " boards  selected");
-		}
-
-
-
-
-
-
-		//var board_object = {board: {name: 'Test dboard', updated_at: '2011-04-11 13:44'}};
-		//  $("#toolbox_board_name").text(board_object.board.name);
+  $("#boards_container li").click(function(e){
+    getData = function(id){
+      $.ajax({
+        url:'/boards/' + id + '.json',
+        type: 'GET',
+        success: function(data, textStatus, jqXHR){
+          toolbox_board_name.text(data.board.name);
+          toolbox_board_info_created.text(data.board.created_at);
+          toolbox_board_info_updated.text(data.board.updated_at);
+          toolbox_board_info_owner.text(data.board.owner_id);
+          
+        }
+      })
+    };
+    
+    $(e.target).toggleClass("selected");
+    var id = $(e.target).attr('id').split('_').pop();
+    var toolbox_board_name = $('#toolbox_board_name');
+    var toolbox_board_info_created = $('#toolbox_info_created');
+    var toolbox_board_info_updated = $('#toolbox_info_updated');
+    var toolbox_board_info_owner = $('#toolbox_info_owner');
+    var toolbox_board_info_name = $('#toolbox_info_name');
+    
+    
+    if($(".selected").length==1){
+      
+      getData($(".selected")[0].id.split('_')[1]);
+      toolbox_board_info_name.text(" ");
+      
+    }else if($(".selected").length==0){
+      toolbox_board_name.text("Name");
+      toolbox_board_info_created.text(" ");
+      toolbox_board_info_updated.text(" ");
+      toolbox_board_info_owner.text(" ");
+      toolbox_board_info_name.text(" ");
+    }else{
+      toolbox_board_info_name.html('');
+      toolbox_board_name.text($(".selected").length + " boards  selected");
+      
+      toolbox_board_info_name.text($(".selected").each(function(){
+        toolbox_board_info_name.append($(this).text() + "<br>")
+        
+      }
+    )
+  )
+  
+  toolbox_board_info_created.text(" ");
+  toolbox_board_info_updated.text(" ");
+  toolbox_board_info_owner.text(" ");
+}
 
 	});
 	$(".toolbox_button_delete").click(function(){
