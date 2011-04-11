991ac2c8ee36d085089cf0628804da1a902e44d5
Alfred Andersson <alfreda@kth.se>
2011-04-11 15:08:36 +0200

    id8Up implementerad i select.board.js

--- public/javascripts/select.board.js
+++ public/javascripts/select.board.js
@@ -1,61 +1,61 @@
 $(document).ready(function(){
   $("#boards_container li").click(function(e){
-    getData = function(id){
-      $.ajax({
-        url:'/boards/' + id + '.json',
-        type: 'GET',
+		getData = function(id){
+			$.ajax({ 
+				url:'/boards/' + id + '.json', 
+				type: 'GET',
         success: function(data, textStatus, jqXHR){
-          toolbox_board_name.text(data.board.name);
+					toolbox_board_name.text(data.board.name);
           toolbox_board_info_created.text(data.board.created_at);
           toolbox_board_info_updated.text(data.board.updated_at);
           toolbox_board_info_owner.text(data.board.owner_id);
-          
-        }
+
+				}		
       })
     };
-    
-    $(e.target).toggleClass("selected");
-    var id = $(e.target).attr('id').split('_').pop();
-    var toolbox_board_name = $('#toolbox_board_name');
-    var toolbox_board_info_created = $('#toolbox_info_created');
+
+		$(e.target).toggleClass("selected");
+		var id = $(e.target).attr('id').split('_').pop();
+		var toolbox_board_name = $('#toolbox_board_name');
+		var toolbox_board_info_created = $('#toolbox_info_created');
     var toolbox_board_info_updated = $('#toolbox_info_updated');
     var toolbox_board_info_owner = $('#toolbox_info_owner');
     var toolbox_board_info_name = $('#toolbox_info_name');
-    
-    
-    if($(".selected").length==1){
-      
-      getData($(".selected")[0].id.split('_')[1]);
+
+
+		if($(".selected").length==1){
+
+			getData($(".selected:eq(0)").id8Up());
       toolbox_board_info_name.text(" ");
       
-    }else if($(".selected").length==0){
-      toolbox_board_name.text("Name");
+		}else if($(".selected").length==0){
+			toolbox_board_name.text("Name");
       toolbox_board_info_created.text(" ");
       toolbox_board_info_updated.text(" ");
       toolbox_board_info_owner.text(" ");
       toolbox_board_info_name.text(" ");
-    }else{
+		}else{
       toolbox_board_info_name.html('');
-      toolbox_board_name.text($(".selected").length + " boards  selected");
+			toolbox_board_name.text($(".selected").length + " boards  selected");
       
       toolbox_board_info_name.text($(".selected").each(function(){
-        toolbox_board_info_name.append($(this).text() + "<br>")
-        
-      }
-    )
+      toolbox_board_info_name.append($(this).text() + "<br>")
+    
+  }
+)
   )
-  
-  toolbox_board_info_created.text(" ");
-  toolbox_board_info_updated.text(" ");
-  toolbox_board_info_owner.text(" ");
-}
+            
+      toolbox_board_info_created.text(" ");
+      toolbox_board_info_updated.text(" ");
+      toolbox_board_info_owner.text(" ");
+		}
 
 	});
 	$(".toolbox_button_delete").click(function(){
 		$(".selected.note").map(function(index, domElement) 
 		{ 
-			var note_id = $(domElement).attr('id').split('_').pop();
-			var board_id = $(domElement).data('board_id').split('_').pop();
+			var note_id = $(domElement).attr('id').id8Up();
+			var board_id = $(domElement).data('board_id').id8Up();
 			var url_path =  "/boards/" + board_id + "/notes/" + note_id;
 
 
@@ -63,7 +63,7 @@ $(document).ready(function(){
 		} );
 		$(".selected.board_container").map(function(index, domElement) 
 		{ 
-			var board_id = $(domElement).attr('id').split('_').pop();
+			var board_id = $(domElement).attr('id').id8Up();
 			var url_path =  "/boards/" + board_id;
 
 
