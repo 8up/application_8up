ab30164de69c8cf365ee2839a54d17c94e3c2cd4
Arash <arash164@gmail.com>
2011-04-26 11:36:10 +0200

    hej
    Signed-off-by: Arash <arash164@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -20,24 +20,6 @@ ActiveRecord::Schema.define(:version => 20110426080754) do
     t.boolean  "in_trash"
   end
 
-  create_table "ckeditor_assets", :force => true do |t|
-    t.string   "data_file_name",                                 :null => false
-    t.string   "data_content_type"
-    t.integer  "data_file_size"
-    t.integer  "assetable_id"
-    t.string   "assetable_type",    :limit => 30
-    t.string   "type",              :limit => 25
-    t.string   "guid",              :limit => 10
-    t.integer  "locale",            :limit => 1,  :default => 0
-    t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "ckeditor_assets", ["assetable_type", "assetable_id"], :name => "fk_assetable"
-  add_index "ckeditor_assets", ["assetable_type", "type", "assetable_id"], :name => "idx_assetable_type"
-  add_index "ckeditor_assets", ["user_id"], :name => "fk_user"
-
   create_table "contents", :force => true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -64,8 +46,8 @@ ActiveRecord::Schema.define(:version => 20110426080754) do
     t.integer  "owner_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "field_id"
     t.boolean  "trashcan"
+    t.integer  "field_id"
     t.string   "color"
   end
 
@@ -84,6 +66,9 @@ ActiveRecord::Schema.define(:version => 20110426080754) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
