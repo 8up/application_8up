31fb3d9f5b924d5090c1f264e778cf9073944388
Erik Ylipää <erik.ylipaa@gmail.com>
2011-04-26 11:05:02 +0200

    ordnade om lite för resize field scripten

--- public/javascripts/field_resize.js
+++ public/javascripts/field_resize.js
@@ -1,35 +1,22 @@
-// Litet plugin för att göra fields resizable
-//Behöver kanske inte vara ett plugin men jag ville prova
-(function($){
-    jQuery.fn.resize8up = function(){
+$(document).ready(function() {
+	$(".resizable8up-handle").mousedown(field_resize_handle);
+    });
 
-	var handler_s = $('<div></div>');
-	handler_s.addClass("resizable8up-handle");
-
-	var handler_n = handler_s.clone();
-	var handler_w = handler_s.clone();
-	var handler_e = handler_s.clone();
-
-	handler_n.addClass("resizable8up-n");
-	handler_w.addClass("resizable8up-w");
-	handler_e.addClass("resizable8up-e");
-	handler_s.addClass("resizable8up-s");
-
-	$(this).append(handler_n);
-	$(this).append(handler_e);
-	$(this).append(handler_w);
-	$(this).append(handler_s);
-
-	//Detta stänger av oönskade beteenden  vid mousedown
-	$(".resizable8up-handle").each(function() {
-		this.onselectstart = function() { return false; };
-		this.onmousedown = function(event) { 
-		    event.preventDefault();
-		    return false; 
-		};
-	    });
-	
-	$("resizeable8up-handle").mousedown();
-
-    }
-})(jQuery);
+function field_resize_handle(event) {
+    //Ta reda på åt vilket håll resize:en är på väg.
+    var direction = null;
+    if ($(event.target).hasClass("resizable8up-n")) {
+	    direction = "north";
+	}
+    else if ($(event.target).hasClass("resizable8up-s")) {
+	    direction = "south";
+	}
+    else if ($(event.target).hasClass("resizable8up-e")) {
+	    direction = "east";
+	}
+    else if ($(event.target).hasClass("resizable8up-w")) {
+	    direction = "west";
+	}
+    
+    return direction;    
+}
