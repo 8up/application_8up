61b327793c93688b563c4805e252fd1a0428a0ad
Andreas Lindfors <alindf@kth.se>
2011-04-27 10:03:08 +0200

    fixat javascript för clickevent på div
    Signed-off-by: Andreas Lindfors <alindf@kth.se>

--- public/javascripts/field_merge.js
+++ public/javascripts/field_merge.js
@@ -3,8 +3,9 @@ $(document).ready(function()
   $(".resizable8up-handle").click(function(e)
   {
     alert("hej");
-    var resizeable8up_handle = $(e.target);
-    var field_1 = resizeable8up_handle.parent();
+    alert("hej2");
+    var resizable8up_handle = $(e.target);
+    var field_1 = resizable8up_handle.parent();
     var field_1_neighbours = field_1.data("neighbours");
     var field_1_neighbours_n = field_1_neighbours["north"];
     var field_1_neighbours_s = field_1_neighbours["south"];
@@ -13,18 +14,21 @@ $(document).ready(function()
     var field_2;
     var merge_direction;
   
-    if (resizeable8up_handle.hasClass("resizeable8up-n") || resizeable8up_handle.hasClass("resizeable8up-s")) 
+    if (resizable8up_handle.hasClass("resizable8up-n") || resizable8up_handle.hasClass("resizable8up-s")) 
     {
-      merge_direction = vertical;
-      if(resizeable8up_handle.hasClass("resizeable8up-n"))
+      alert("vert");
+      
+      if(resizable8up_handle.hasClass("resizable8up-n"))
       {
+        alert("testvert");
         if(field_1_neighbours_n.length == 1)
         {
-          field_2 = "field_" + field_1_neighbours_n;
+          field_2 = $(("#field_" + field_1_neighbours_n[0]));
+          merge_direction = "north";
         }
         else if(field_1_neighbours_n.length > 1) 
         {
-          
+          alert("Kan inte merga dessa fält!");
         }
 
       }
@@ -32,28 +36,33 @@ $(document).ready(function()
       {
         if(field_1_neighbours_s.length == 1) 
         {
-          field_2 = "field_" + field_1_neighbours_s;
+          alert("testvert");
+          field_2 = $(("#field_" + field_1_neighbours_s[0]));
+          merge_direction = "south";
         }
         else if(field_1_neighbours_s.length > 1) 
         {
-          
+          alert("Kan inte merga dessa fält!");
         }
         
       }
         
     }
-    else if (resizeable8up_handle.hasClass("resizeable8up-w") || resizeable8up_handle.hasClass("resizeable8up-e")) 
+    else if (resizable8up_handle.hasClass("resizable8up-w") || resizable8up_handle.hasClass("resizable8up-e")) 
     {
-      merge_direction = horizontal;
-      if(resizeable8up_handle.hasClass("resizeable8up-w")) 
+      alert("horiz");
+      
+      if(resizable8up_handle.hasClass("resizable8up-w")) 
       {
         if(field_1_neighbours_w.length == 1)
         {
-          field_2 = "field_" + field_1_neighbours_w;
+          alert("testhoriz");
+          field_2 = $(("#field_" + field_1_neighbours_w[0]));
+          merge_direction = "west";
         }
         else if(field_1_neighbours_w.length > 1) 
         {
-          
+          alert("Kan inte merga dessa fält!");
         }
     
       }
@@ -61,16 +70,18 @@ $(document).ready(function()
       {
         if(field_1_neighbours_e.length == 1) 
         {
-          field_2 = "field_" + field_1_neighbours_e;
+          alert("testhoriz");
+          field_2 = $(("#field_" + field_1_neighbours_e[0]));
+          merge_direction = "east";
         }
         else if(field_1_neighbours_e.length > 1) 
         {
-        
+        alert("Kan inte merga dessa fält!");
         }
     
       }  
     }
-    
+    alert("fieldmerge");
     field_merge(field_1, field_2, merge_direction);
   });
 }); 
@@ -83,28 +94,28 @@ function field_merge(field_1, field_2, merge_direction)
   var new_height = field_1.height() + field_2.height();
   var new_width = field_1.width() + field_2.width();
     
-  if (merge_direction = "north")
+  if (merge_direction == "north")
     {
     field_2.height(new_height);
       $.ajax({url: "/boards/" + board_id  + 
 		"/merge_fields/" +  field_id , type:"POST", 
 		data:{field_to_enlarge : field_2, field_to_delete : field_1, merge_direction : "vertical"} , success: function(data) {}})
     }
-  if (merge_direction = "west")
+  if (merge_direction == "west")
     {
     field_2.width(new_width);
       $.ajax({url: "/boards/" + board_id  + 
 		"/merge_fields/" +  field_id , type:"POST", 
 		data:{field_to_enlarge : field_2, field_to_delete : field_1, merge_direction : "horizontal"} , success: function(data) {}})
-}
-  if (merge_direction = "south")
+    }
+  if (merge_direction == "south")
     {
     field_1.height(new_height);  
       $.ajax({url: "/boards/" + board_id  + 
 		"/merge_fields/" +  field_id , type:"POST", 
 		data:{field_to_enlarge : field_1, field_to_delete : field_2, merge_direction : "vertical"} , success: function(data) {}})
     }
-  if (merge_direction = "east")
+  if (merge_direction == "east")
     {
     field_1.width(new_width);
       $.ajax({url: "/boards/" + board_id  + 
