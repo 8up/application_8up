94281a7faf4fd12c11acd02201b42c497d10b3ee
Arash <arash164@gmail.com>
2011-04-28 14:39:10 +0200

    Mekkar i Settings. Nu finns edit_password och change_password
    Signed-off-by: Arash <arash164@gmail.com>

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -4,5 +4,35 @@ class SettingsController < ApplicationController
   def index
   @hide_toolbox = true
   @hide_addbutton = true
-  end
 end
+
+  def edit_password
+
+  end
+
+  def change_password
+
+  @user = current_user 
+
+  if request.post? 
+  #if User.authenticate(@user.username, 
+  #  params[:password][:old_password]) == @user 
+  
+  @user.password = params[:password][:new_password] 
+  @user.password_confirmation = 
+    params[:password][:new_password_confirmation] 
+  
+  if @user.save 
+    flash[:notice] = 'Your password has been changed' 
+      redirect_to :action => 'edit_password' 
+else 
+    flash[:error] = 'Unable to change your password' 
+end 
+
+else 
+    flash[:error] = 'Invalid password' 
+    end 
+  end 
+end 
+  
+#end
