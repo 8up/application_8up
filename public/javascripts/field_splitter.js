a89f09d2e876a5abb4d5a97d1c3742ece1e69b60
Erik Ylipää <erik.ylipaa@gmail.com>
2011-05-02 10:27:27 +0200

    Nu skickar klienten koordinaterna för var splitten skall göras till servern och delar sin div på rätt ställe

--- public/javascripts/field_splitter.js
+++ public/javascripts/field_splitter.js
@@ -9,6 +9,7 @@ $(document).ready(function() {
 function do_split(e) {
     var field = $(this)
     var direction = $("#split_button_table").data("split_direction");
+    
 
     //Om direction varken är vertical eller horizontal gör vi inget
     if (!(direction == "vertical" || direction == "horizontal")) {
@@ -16,31 +17,35 @@ function do_split(e) {
     }
     var board_id =field.parent().id8Up(); 
     var field_id = field.id8Up();                         ;
-    
+   
     var new_field = field.clone();
     new_field.html(''); //radera innehållet i klonen
     new_field.attr("id", "");
-
+    var split_position = 0;
+    field.after(new_field); //lägger till det nya fältet i html-koden efter det ursprungliga   
     if (direction == "vertical") {
-	var new_width = field.width()/2;
-	new_field.width(new_width);
+	split_position = Math.round(e.pageX - field.offset().left);
+	var new_width = split_position;
 	field.width(new_width);
-    	new_field.offset({left:new_width}); //adderar positionen till det nya fältet
+	new_field.width(new_field.width() - field.width());
+    	new_field.offset({left:new_width + field.offset().left}); //adderar positionen till det nya fältet
     }
     else if (direction == "horizontal") {
-	var new_height = field.height()/2;
-	new_field.height(new_height);
+	split_position = Math.round(e.pageY - field.offset().top);
+	var new_height = split_position;
 	field.height(new_height);
-	new_field.offset({top: new_height}); //adderar positionen till det nya fältet
+	new_field.height(new_field.height() - field.height());
+
+	new_field.offset({top: new_height + field.offset().top}); //adderar positionen till det nya fältet
     }
 
-    field.after(new_field); //lägger till det nya fältet i html-koden efter det ursprungliga   
+  
     attach_field_handlers(new_field); //lägg till hanterare till det nya fältet
     
     $.ajax({url: "/boards/" + board_id  + 
 		"/split_field/" +  field_id , type:"POST", 
 		data:{
-		split_direction : direction} , 
+		split_direction : direction, split_position: split_position } , 
 		success: function(data) {
 		new_field.attr("id", "field_" + data.new_field.field.id); 
 		    // Här kommer vi måsta lägga in kod för att 
