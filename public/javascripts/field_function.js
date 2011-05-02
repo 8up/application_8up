20ae008a8cf2afb2e2137ed6190df780b16b87d6
Johan Tjelldén <tjelle@gmail.com>
2011-05-02 11:36:37 +0200

    Nu har vi en fin redigerare på framsidan av en note.
    Signed-off-by: Johan Tjelldén <tjelle@gmail.com>

--- public/javascripts/field_function.js
+++ public/javascripts/field_function.js
@@ -19,23 +19,11 @@ function attach_field_handlers(field) {
 
 function update(event, ui, field_id){
 	var note_data = {}
-	var src_field = $(event.srcElement.parentElement);
 	var dest_field = $(event.target);
+	          
 	
-	var start_page_x = ui.draggable.data('startPageX');
-	var start_page_y = ui.draggable.data('startPageY');	
-	var diff_left =  event.pageX - start_page_x;
-	var diff_top =  event.pageY - start_page_y;
-
-	var start_left = ui.draggable.data('startLeft');
-	var start_top = ui.draggable.data('startTop');
-	var dest_left = dest_field.offset().left;
-	var dest_top = dest_field.offset().top;
-	var src_left = src_field.offset().left;
-	var src_top = src_field.offset().top;
-	
-	note_data["position_x"] = (src_left + start_left) + diff_left - dest_left;
-	note_data["position_y"] = (src_top + start_top) + diff_top - dest_top;
+  note_data["position_x"] = ui.offset.left - dest_field.offset().left;
+  note_data["position_y"] = ui.offset.top - dest_field.offset().top;
 	
 	note_data["field_id"] = field_id;
  	id = ui.helper.context.id.split('_').pop();
