188da506139c3235d4a6ebe3d9f411acbf7064a9
Erik Ylipää <erik.ylipaa@gmail.com>
2011-05-03 14:36:24 +0200

    Fixade så att boards i overview inte är i cont-divvar

--- public/javascripts/select.js
+++ public/javascripts/select.js
@@ -46,7 +46,7 @@ $(document).ready(
 			de_select_all(e, this);
 		});
 		
-		$("#cont div").click(function(e){
+		$(".board_container").click(function(e){
 			select(e,$(this).closest("div"));
 		});
 	}
