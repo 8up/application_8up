05ea492c88cd9a883d1877ee84f3e9a322db63a1
Johan Tjelldén <tjelle@gmail.com>
2011-05-03 17:15:22 +0200

    Toolbaren ska fungera på rätt sätt.
    Signed-off-by: Johan Tjelldén <tjelle@gmail.com>

--- public/javascripts/slide.js
+++ public/javascripts/slide.js
@@ -1,9 +1,20 @@
 $(document).ready(
 	function(){
+	  var old_width = $('#workspace').width();
+	  var toolbox_width = $('#toolbox_container').width();
+	  $('#workspace').css('margin-left', toolbox_width + 'px');
+	  
+	  $('#workspace').width($(window).width() - toolbox_width); 
+    
 		$("#toolbox_area_2").click(function(){
 			$("div#toolbox_area_1").toggle();
 			if(window.page_context == 'start_page'){
-			  $('#content').css('margin-left', $('#toolbox_container').width() + 'px');
+			  var old_width = $('#workspace').width();
+			  var toolbox_width = $('#toolbox_container').width();
+			  $('#workspace').css('margin-left', toolbox_width + 'px');
+			  
+			  $('#workspace').width($(window).width() - toolbox_width); 
+			  
 			}
 			var panel_arrow=$("#panel_arrow");
 			if (panel_arrow.attr("src")== "/images/panel_button_right.png") {
@@ -12,5 +23,7 @@ $(document).ready(
 				panel_arrow.attr("src", "/images/panel_button_right.png");
 			}
 		});
+		/*$("#toolbox_area_2").trigger('click');*/
 	}
+	
 );
