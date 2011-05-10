c7332aeaba36ea3ace66d9cb8d8f76d59305e4a2
Erik Ylipää <erik.ylipaa@gmail.com>
2011-05-10 16:35:49 +0200

    Fixat så att faye använder johans amazon-instans

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :authenticate_user!, :set_last_request_at
+  before_filter :set_faye_params, :authenticate_user!, :set_last_request_at
  # before_filter :set_last_request_at
   layout(:get_layout_name)
-  
+   
+  def set_faye_params
+    @faye_adress = "46.51.179.63"
+    # @faye_adress = "localhost"
+    @faye_port = "9292"
+  end
   def get_layout_name
-    if devise_controller? && action_name != 'edit' && action_name != 'update'
+        if devise_controller? && action_name != 'edit' && action_name != 'update'
       "log_in"
     else
       "application"
@@ -18,8 +23,8 @@ class ApplicationController < ActionController::Base
   end
 
   def publish_to_faye(channel, data, action)
-    faye_port = 9292
-    faye_url = "http://localhost:#{faye_port}/faye"
+    faye_url = "http://#{@faye_adress}:#{@faye_port}/faye"
+    #faye_url = "http://localhost:9292/faye"
     message = {:channel => channel, :data => {:action => action, :data => data} }
     uri = URI.parse(faye_url)  
     Net::HTTP.post_form(uri, :message => message.to_json) 
